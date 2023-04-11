7ccf62b87f6abae7f75d62e5914784f01854efb6
PogBlaze <fd.costa@campus.fct.unl.pt>
2023-04-11 16:53:58 +0100

    Third update

---
+++ set_vars.cmd
@@ -0,0 +1,7 @@
+set DATASTORE_DATASET=strong-surfer-379310
+set DATASTORE_EMULATOR_HOST=localhost:8081
+set DATASTORE_EMULATOR_HOST_PATH=localhost:8081/datastore
+set DATASTORE_HOST=http://localhost:8081
+set DATASTORE_PROJECT_ID=strong-surfer-379310
+
+set DATASTORE_USE_PROJECT_ID_AS_APP_ID=true
